c80b731f4fa6913548c52515efdc860164e61850
Vlad <vladlav@hotmail.com>
2020-10-24 16:42:41 +1300

    Initial commit

---
+++ src/main/java/com/vlad/postsreader/PostsReaderApplication.java
@@ -0,0 +1,13 @@
+package com.vlad.postsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlad/postsreader/PostsReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlad.postsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
